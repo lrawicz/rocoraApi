79dd3745f243050c253daf004dfb9cb3815ec51e
lrawicz <lrawicz@gmail.com>
2025-08-20 14:55:25 -0300

    review test02

--- test/02-testLocation.ts
+++ test/02-testLocation.ts
@@ -1,76 +1,89 @@
-//test location
 import request from "supertest";
 import  app  from "../src/app"; // Adjust the path to your app file
-import config from "../src/config/config";
 import AppDataSource from "../src/dataSource";
 import { beforeAll, afterAll, describe, it, expect } from '@jest/globals';
 import { locationStatus } from "../src/entity/location";
 
+let authToken: string;
+const buildings:{direction: string, id?: number}[] = [{direction: "Almirante Brown 1234"}];
+const locations:Partial<{id:number,name: string, buildingId: number,status:locationStatus,additionalInfo: string}>[]= 
+    [{name:"AB01-T2"}];
+
 beforeAll(async () => {
     await AppDataSource.initialize();
-});
 
-afterAll(async () => {
-    await AppDataSource.destroy();
-});
+    // Perform login to get JWT
+    const loginResponse = await request(app)
+        .post("/api/auth/login")
+        .send({ username: "admin", password: "admin" });
 
-let buildings:{direction: string, id?: number}[] = [{direction: "Almirante Brown 123"}];
-let locations:Partial<{id:number,name: string, buildingId: number,status:locationStatus,additionalInfo: string}>[]= 
-    [{name:"AB01"}];
-describe("Locations API", () => {
-    it("should create a new building", async () => {
-        const createResponse = await request(app)
+    expect(loginResponse.status).toBe(200);
+    expect(loginResponse.body).toHaveProperty("token");
+    authToken = loginResponse.body.token;
+
+
+    const createResponse = await request(app)
             .post("/api/building")
-            .set("X-API-Key", config.API_KEY)
+            .set("Authorization", `Bearer ${authToken}`)
             .send(buildings[0]);
         buildings[0].id = createResponse.body.id;
         locations[0].buildingId = createResponse.body.id;
         expect(createResponse.status).toBe(201);
-        expect(createResponse.body.direction).toBe("Almirante Brown 123");
-    });
+        expect(createResponse.body.direction).toBe(buildings[0].direction);
+});
 
+afterAll(async () => {
+
+    await AppDataSource.destroy();
+});
+
+
+describe("Locations API", () => {
     it("should create a new location", async () => {
         const createResponse = await request(app)
             .post("/api/location")
-            .set("X-API-Key", config.API_KEY)
+            .set("Authorization", `Bearer ${authToken}`)
             .send(locations[0]);
         expect(createResponse.status).toBe(201);
         locations[0].id = createResponse.body.id;
-        expect(createResponse.body.name).toBe("AB01");
+        expect(createResponse.body.name).toBe(locations[0].name);
     });
+
     it("should get AB01 by ID", async () => {
         const createResponse = await request(app)
             .get(`/api/location/${locations[0].id}`)
-            .set("X-API-Key", config.API_KEY)
+            .set("Authorization", `Bearer ${authToken}`)
             .send(locations[0]);
         expect(createResponse.status).toBe(200);
-        expect(createResponse.body.name).toBe("AB01");
-        expect(createResponse.body.status).toBe("ACTIVE");
+        expect(createResponse.body.name).toBe(locations[0].name);
+        expect(createResponse.body.status).toBe("ALQUILADO");
+    return;
+
     });
     it("should update AB01 to ABO02", async () => {
-        locations[0].name = "AB02";
-        locations[0].status = "ACTIVE";
+        locations[0].name = "AB02-T3";
+        locations[0].status  = "ALQUILADO" as locationStatus;
         locations[0].additionalInfo = "Test location info";
         const createResponse = await request(app)
             .put(`/api/location/${locations[0].id}`)
-            .set("X-API-Key", config.API_KEY)
+            .set("Authorization", `Bearer ${authToken}`)
             .send(locations[0]);
         expect(createResponse.status).toBe(200);
-        expect(createResponse.body.name).toBe("AB02");
-        expect(createResponse.body.status).toBe("ACTIVE");
+        expect(createResponse.body.name).toBe(locations[0].name);
+        expect(createResponse.body.status).toBe(locations[0].status);
     });
 
     it("delete Location - AB02", async () => {
         const createResponse = await request(app)
             .delete(`/api/location/${locations[0].id}`)
-            .set("X-API-Key", config.API_KEY)
+            .set("Authorization", `Bearer ${authToken}`)
         expect(createResponse.status).toBe(204);
     });
 
     it("delete Building ", async () => {
-        const createResponse = await request(app)
-            .delete(`/api/building/${locations[0].buildingId}`)
-            .set("X-API-Key", config.API_KEY)
-        expect(createResponse.status).toBe(204);
+                const createResponse = await request(app)
+                .delete(`/api/building/${locations[0].buildingId}`)
+                .set("Authorization", `Bearer ${authToken}`)
+            expect(createResponse.status).toBe(204);
     });
 });
