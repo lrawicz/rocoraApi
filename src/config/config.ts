135092ba9963b881ac5ed9ba0ba846380cdb5081
lrawicz <lrawicz@gmail.com>
2025-08-20 14:04:53 -0300

    add jwt and salt variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,8 @@ dotenv.config();
 const config = {
     PORT : process.env.PORT || 3000,
     API_KEY: process.env.API_KEY || "esta-es-una-clave-muy-secreta-y-dificil-de-adivinar",
+    JWT_SECRET: process.env.JWT_SECRET || "supersecretjwtkey",
+    SALT: process.env.SALT || "supersecretSaltkey",
 }
 
 export default config;
