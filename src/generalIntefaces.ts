7732b1114e2a254d9362da5934a1cfe3ef7966a3
lrawicz <lrawicz@gmail.com>
2025-08-30 11:04:42 -0300

    add T to parametersOptions

--- src/generalIntefaces.ts
+++ src/generalIntefaces.ts
@@ -9,20 +9,23 @@ export type ErrorType = {
     statusCode: number;
 }
 
-export type parameterOption = {
-    columnName:string,
-    typeOf:"string"|"number"|"boolean"|"date"|"entity",
+export type parameterOption<T> = {
+    columnName: keyof T,
+    typeOf:"string"|"number"|"boolean"|"date"|"entity"|"enum",
     criteria:{
-        in?:number[],
+        in?:number[]|string[],
         min?:number|Date;
-        minOrEqual?:number|Date;
         max?:number|Date;
-        maxOrEqual?:number|Date;
-        value?:string|number|boolean
+        regex?:string;
+        like?:string;
+        value?:string|number;
+
+        //minOrEqual?:number|Date;
+        //maxOrEqual?:number|Date;
     },    
 }
-export type optionsGetAll = {
-    parameterOptions?:parameterOption[]
+export type optionsGetAll<T> = {
+    parameterOptions?:parameterOption<T>[]
     page:number,
     limit:number
 }
